614ab43f124180252a6e085eb9d4539b5fe386a4
Bruna Lima <t_brunao@hapvida.com.br>
2021-07-24 17:43:23 -0300

    Listagem de clientes para serviços prestados

--- src/app/servicoprestado/servicoprestado-routing.module.ts
+++ src/app/servicoprestado/servicoprestado-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
+import { ServicoPrestadoListComponent } from './servico-prestado-list/servico-prestado-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'servico-prestado-form', component: ServicoPrestadoFormComponent},
+  {path: 'servico-prestado-listagem', component: ServicoPrestadoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
